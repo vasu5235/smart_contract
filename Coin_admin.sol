135682d7cb0e6feac0e773d5bdd8997d9e9da52d
vasu5235 <vasu5235@gmail.com>
2017-07-05 14:30:33 +0530

    Initial commit-Added standard contracts

---
+++ Coin_admin.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.0;
+
+contract Coin {
+    //0x0ef1ada008aeb134b13a8fa2953604b81433e1d0
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function Coin() {
+        minter = msg.sender;
+    }
+
+    function mint(address admin_minter, uint amount){
+        if (msg.sender != minter) return;
+        balances[admin_minter] += amount;
+    }
+
+    function send(address receiver, uint amount) {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, receiver, amount);
+    }
+} 
